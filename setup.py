5ad6f05f4f0c2b32fffdfc864d1784018a4e99f3
matt-git <mattmccullo@gmail.com>
2015-04-03 13:13:15 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        #'sample': ['package_data.dat'],
+        'xlwt': ['installed-files.txt'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
